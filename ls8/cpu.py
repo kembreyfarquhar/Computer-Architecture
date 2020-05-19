b5b99d865dca90091597749809362e66a94ddd74
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-19 16:26:49 -0500

    runs program via command line

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,26 +11,16 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
 
-    def load(self):
+    def load(self, path):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        file = open(path, "r")
+        for line in file:
+            if line[:8][0] == "0" or line[:8][0] == "1":
+                self.ram[address] = int(line[:8], 2)
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,10 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
-cpu.run()
+if len(sys.argv) > 1:
+    cpu.load(sys.argv[1])
+
+    cpu.run()
+
+else:
+    print('Invalid program')
