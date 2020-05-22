b780dc20b22bb584c3d7c9789c072978ab594d0d
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-21 22:09:39 -0500

    adds call and ret functionality

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,12 +10,19 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.sp = 7  # stack pointer index in register
+        self.reg[self.sp] = 244  # stack pointer
 
         self.branchtable = {}
         self.branchtable[130] = self.LDI
         self.branchtable[71] = self.prnt
         self.branchtable[162] = self.mul
+        self.branchtable[160] = self.add
         self.branchtable[1] = self.HLT
+        self.branchtable[69] = self.push
+        self.branchtable[70] = self.pop
+        self.branchtable[17] = self.ret
+        self.branchtable[80] = self.call
 
     def load(self, path):
         """Load a program into memory."""
@@ -35,7 +42,6 @@ class CPU:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        #AND, CMP, DEC, DIV, INC, MOD, MUL, NOT, OR, SHL, SHR, SUB, XOR
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,9 +68,36 @@ class CPU:
     def ram_write(self, MDR, MAR):
         self.ram[MAR] = MDR
 
+    def add(self):
+        self.alu("ADD", self.ram_read(self.pc+1), self.ram_read(self.pc+2))
+        self.pc += 3
+
     def mul(self):
         self.alu("MUL", self.ram_read(self.pc+1), self.ram_read(self.pc+2))
         self.pc += 3
+
+    def push(self, value=None):
+        self.reg[self.sp] -= 1  # decrement sp
+        if not value:
+            value = self.reg[self.ram_read(self.pc + 1)]
+        self.ram_write(value, self.reg[self.sp])
+        self.pc += 2
+
+    def pop(self):
+        value = self.ram_read(self.reg[self.sp])
+        reg_position = self.ram_read(self.pc + 1)
+        self.reg[reg_position] = value
+        self.reg[self.sp] += 1  # increment sp
+        self.pc += 2
+
+    def call(self):
+        new_pc = self.reg[self.ram_read(self.pc+1)]
+        self.push(self.pc + 2)
+        self.pc = new_pc
+
+    def ret(self):
+        self.pc = self.ram_read(self.reg[self.sp])
+        self.reg[self.sp] += 1
 
     def trace(self):
         """
